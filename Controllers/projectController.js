118b47c0eb702144d0880118a3db58ce15285194
Semaj Primm <semajthomasprimm@users.noreply.github.com>
2022-03-06 15:24:53 -0500

    fixed controller bug

--- Controllers/projectController.js
+++ Controllers/projectController.js
@@ -9,10 +9,30 @@ exports.getProject = async (req, res) => {
     }
 }
 
+exports.getProjects = async (req, res) => {
+    try {
+        let response = await Projects.getProjects();
+    
+        res.status(200).json(response.rows[0]);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+}
+
 exports.createProject = async (req, res) => {
     const project = req.body.project;
     try {
         let response = await Projects.createProject(project);
+        res.status(200).json(response.rows[0])
+    } catch (err) {
+        res.status(500).json(err)
+    }
+};
+
+exports.updateProject = async (req, res) => {
+    const project = req.body.project
+    try {
+        let response = await Projects.updateProject(project);
         res.status(200).json(response.rows[0])
     } catch (err) {
         res.status(500).json(err)
--- index.js
+++ index.js
@@ -8,5 +8,15 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// configure routes
+const taskRoutes = require('./Routes/taskRoutes');
+const projectRoutes = require('./Routes/projectRoutes');
+const statusRoutes = require('./Routes/statusRoutes');
+
+// run routes on endpoints
+
+app.use('/tasks', taskRoutes);
+app.use('/projects', projectRoutes);
+app.use('/status', statusRoutes);
 
 app.listen(process.env.PORT || 4000, () => console.log(`listening at http://localhost:${process.env.PORT || 4000}`));
